817f62f1e54cbf4900806dad4e467cebf909cb15
Daniel <dan.tapiat@alumnos.duoc.cl>
2018-10-30 17:56:33 -0300

    Views update

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,4 @@ from django.conf.urls import url,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^usuario/$', include('Usuarios.urls')),
-    url('', include('Usuarios.urls')),
 ]
