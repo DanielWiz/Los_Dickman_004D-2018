baad63133e03ec25174b2dbd08f6fff3f4afafa1
Daniel <dan.tapiat@alumnos.duoc.cl>
2018-10-30 17:59:24 -0300

    Update urls.py

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.inicio, 'template_name': 'usuarios/inicio.html'),
+    url(r'$',views.inicio, {'template_name': 'usuarios/inicio.html'}),
     url(r'^login/$', views.login , {'template_name': 'usuarios/login.html'}),
     url(r'^logout/$', views.logout , {'template_name': 'usuarios/logout.html'}),
     url(r'^registro/$', views.registro,name='registro'),
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import url,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^usuario/$', include('Usuarios.urls')),
+    url(r'', include('Usuarios.urls')),
 ]
