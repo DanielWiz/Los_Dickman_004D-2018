0b9e761f22ea9a5f497bd1ab8e8851dbb33c3feb
Daniel <dan.tapiat@alumnos.duoc.cl>
2018-11-27 17:43:40 -0300

    Sevice Worker APW

--- Usuarios/static/js/app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./static/js/service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
