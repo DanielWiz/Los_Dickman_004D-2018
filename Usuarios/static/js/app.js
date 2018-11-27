e22eca140ba946a91aa6febb962723502f1c8e09
Daniel <dan.tapiat@alumnos.duoc.cl>
2018-11-27 17:11:17 -0300

    Update ServiceWorker 2

--- Usuarios/static/js/app.js
+++ Usuarios/static/js/app.js
@@ -1,5 +1,8 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
+navigator.serviceWorker.register("/serviceworker.js").then(registration => {
+    console.log("success!");
+    if (registration.installing) {
+      registration.installing.postMessage("Howdy from your installing page.");
+    }
+  }, err => {
+    console.error("Installing the worker failed!", err);
+  });
