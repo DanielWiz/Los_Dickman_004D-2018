e77666e77ee9ce5e5e4e099f8bf3ca14972ffb54
Daniel <dan.tapiat@alumnos.duoc.cl>
2018-11-27 17:36:18 -0300

    Updata app.js

--- Usuarios/static/js/app.js
+++ Usuarios/static/js/app.js
@@ -1,8 +1,5 @@
-navigator.serviceWorker.register("./static/js/service-worker.js").then(registration => {
-    console.log("success!");
-    if (registration.installing) {
-      registration.installing.postMessage("Howdy from your installing page.");
-    }
-  }, err => {
-    console.error("Installing the worker failed!", err);
-  });
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./static/js/service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
